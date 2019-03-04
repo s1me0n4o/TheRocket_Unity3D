5f3b212fc0695f33307a613e662c4c29727d3684
S.Vasilev <s.vasilev9169@gmail.com>
2019-03-04 02:14:11 +0200

    Default Level One

---
+++ Assets/Scripts/Collisions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collisions : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        if (!other.gameObject.CompareTag("Friendly"))// || Fluel == 0)
+        {
+            print("Game Over");
+            //destroy rocket
+            //reload the level after 2 secs
+        }
+        else if (other.gameObject.CompareTag("Finish"))
+        {
+            print("You Win");
+        }
+                
+    }
+
+}
--- Assets/Scripts/RocketMovement.cs
+++ Assets/Scripts/RocketMovement.cs
@@ -8,33 +8,56 @@ public class RocketMovement : MonoBehaviour
     Rigidbody rb;
     Vector3 upFroce = Vector3.up; // .up is equal to Y axes
     Vector3 leftForce = Vector3.forward; // .forward is equal to Z axes
+    AudioSource rocketSound;
+
+    [SerializeField] float thrust = 200;
+    [SerializeField] float upThrust = 2000f;
+    //public float thrust = 150f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rocketSound = GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        ProcessInput();
-
+        PowerAndAudio();
+        RocketRotation();
     }
 
-    private void ProcessInput()
+    //MovementUpside and audio
+    private void PowerAndAudio()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddRelativeForce(upFroce);
+            rb.AddRelativeForce(upFroce * upThrust);
+            rocketSound.enabled = true;
         }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rocketSound.enabled = false;
+        }
+    }
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            // We can use the Z axes to rotate around 
-            transform.Rotate(-leftForce); 
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(leftForce);
-        }
+    //Rotation
+    private void RocketRotation()
+    {
+        rb.freezeRotation = true; // freeze the rotation before taking manual contorl in order to freze the rotation in case we hit something
+
+        float rotationSpeed = thrust * Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                // We can use the Z axes to rotate around 
+
+                transform.Rotate(-leftForce * rotationSpeed);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(leftForce * rotationSpeed);
+            }
+
+        rb.freezeRotation = false; //resume physics control of rotation
     }
 }
